b963ece12e3b6e6ee5435f14d39b63ecb9dbc324
prudhvipotnuru <prudhvipotnuru1998@gmail.com>
2018-08-03 01:20:33 +0530

    initial commit

--- opensecret/settings.py
+++ opensecret/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'yhx9ek@r-oy@u$h*+%!e!jop542*+%*nv(tr@_$6z8he47)-o4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['prudhvi1998.herokuapp.com','127.0.0.1','localhost']
 
 
 # Application definition
